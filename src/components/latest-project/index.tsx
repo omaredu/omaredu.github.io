681e454e07f8672f217e1d3dfff3a553cddb82ce
Omar Sánchez <me@omaredu.com>
2026-01-18 21:04:28 -0600

    feat: replace tempo project with payway

--- src/components/latest-project/index.tsx
+++ src/components/latest-project/index.tsx
@@ -6,38 +6,40 @@ export default function LatestProject(props: LatestProjectProps) {
   return (
     <section
       {...props}
-      style={{ background: "linear-gradient(0deg, #479CFF, #47B5FF)" }}
+      style={{ background: "linear-gradient(0deg, #71A726, #86BD36)" }}
       className={`flex flex-col overflow-hidden items-center outline outline-1 outline-border/10 md:flex-row md:justify-center ${props.className}`}
     >
       <div className="flex flex-col items-center md:items-start md:gap-[60px] md:w-[350px] md:mr-[45px]">
         <img
           className="h-[30px] my-[30px]"
-          src="/tempo.svg"
-          alt="Tempo logo white"
+          src="/payway.svg"
+          alt="Payway logo white"
         />
         <div className="flex flex-col items-center md:items-start">
           <label className="text-sm px-[8px] py-[3px] text-white bg-white/20 rounded-full font-semibold mb-[10px]">
-            Work in Progress
+            Launched
           </label>
           <h1 className="text-white text-center md:text-left px-[20%] md:px-0">
-            Tempo, an Open Source Weather App built with React Native
+            Payway. Corporate cards and payroll, finally in one place.
           </h1>
           <p className="hidden md:block text-white/70">
-            Tempo is an open source weather app built with React Native. It
-            delivers accurate weather information with a user-friendly
-            interface.
+            We are bringing corporate cards and payroll together in a simple,
+            intuitive experience.
           </p>
-          <a href="/Tempo" target="_blank">
-            <Button className="my-[25px]">Discover Tempo</Button>
+          <a
+            href="https://apps.apple.com/mx/app/payway/id6749019341"
+            target="_blank"
+          >
+            <Button className="my-[25px]">Discover Payway</Button>
           </a>
         </div>
       </div>
       <div className="flex flex-col items-center w-[240px] relative border border-border/10 rounded-[30px] p-[6px] bg-white -mb-[85px] md:translate-y-10">
-        <div className="absolute top-[21px] h-[20px] w-[60px] rounded-full bg-white border-border/10" />
+        <div className="absolute top-[21px] h-[20px] w-[60px] rounded-full bg-white border border-border/10" />
         <img
           className="rounded-[24px]"
-          src="/tempo-ss.png"
-          alt="Tempo app screenshot"
+          src="/payway-ss.png"
+          alt="Payway app screenshot"
         />
         <div className="absolute bottom-[11px] h-[5px] w-[70px] opacity-20 rounded-full bg-white border-border/10" />
       </div>
