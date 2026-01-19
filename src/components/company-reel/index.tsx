7d331effbc44c9032f2198605680b26b1601242c
Omar Sánchez <me@omaredu.com>
2026-01-19 03:25:11 -0600

    feat: add alt, aria-label and noopener to company links

--- src/components/company-reel/index.tsx
+++ src/components/company-reel/index.tsx
@@ -10,23 +10,41 @@ export default function CompanyReel(props: CompanyReelProps) {
         <a
           href="https://www.udem.edu.mx/"
           className="hover:opacity-60 transition"
+          aria-label="Go to University of Monterrey's website"
           target="_blank"
+          rel="noopener noreferrer"
         >
-          <img src="/organizations/udem.png" className="h-[16px]" />
+          <img
+            src="/organizations/udem.png"
+            alt="University of Monterrey"
+            className="h-[16px]"
+          />
         </a>
         <a
           href="https://www.msgova.com.mx/"
           className="hover:opacity-60 transition"
+          aria-label="Go to Gova Multiservices' website"
           target="_blank"
+          rel="noopener noreferrer"
         >
-          <img src="/organizations/gova.png" className="h-[25px]" />
+          <img
+            alt="Gova Multiservices"
+            src="/organizations/gova.png"
+            className="h-[25px]"
+          />
         </a>
         <a
           href="https://tecmilenio.mx/"
           className="hover:opacity-60 transition"
+          aria-label="Go to Tecmilenio University's website"
           target="_blank"
+          rel="noopener noreferrer"
         >
-          <img src="/organizations/tecmilenio.png" className="h-[25px]" />
+          <img
+            alt="Tecmilenio University"
+            src="/organizations/tecmilenio.png"
+            className="h-[25px]"
+          />
         </a>
       </div>
     </section>
